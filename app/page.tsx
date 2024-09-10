63180b924bdee0ecb8723c033b94524fb74919d4
QuiLion7 <quilbrub@gmail.com>
2024-09-09 23:37:58 -0300

    feat: add missing components to the Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
 import Banner from "@/components/banner";
 import Contact from "@/components/contact";
+import Faturamento from "@/components/faturamento";
+import Fibonacci from "@/components/fibonacci";
+import Inversao from "@/components/inversao";
 import Nav from "@/components/nav";
+import Soma from "@/components/soma";
+import Valor from "@/components/valor";
 
 export default function Home() {
   return (
     <div className="w-full h-full overflow-x-primary">
       <Nav />
       <Banner />
-
+      <Soma />
+      <Fibonacci />
+      <Faturamento />
+      <Valor />
+      <Inversao />
       <Contact />
     </div>
   );
